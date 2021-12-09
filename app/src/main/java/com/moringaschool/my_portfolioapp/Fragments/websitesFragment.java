47a094b66c6a31b67456f4739343fc6d622bae8a
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-12-09 12:32:23 +0300

    Afew changes

---
+++ app/src/main/java/com/moringaschool/my_portfolioapp/Fragments/websitesFragment.java
@@ -0,0 +1,26 @@
+package com.moringaschool.my_portfolioapp.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.moringaschool.my_portfolioapp.R;
+
+
+public class websitesFragment extends Fragment {
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view =  inflater.inflate(R.layout.fragment_websites, container, false);
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/moringaschool/my_portfolioapp/PortfolioActivity.java
+++ app/src/main/java/com/moringaschool/my_portfolioapp/PortfolioActivity.java
@@ -1,23 +1,37 @@
 package com.moringaschool.my_portfolioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.moringaschool.my_portfolioapp.Fragments.AndroidAppsFragment;
+import com.moringaschool.my_portfolioapp.Fragments.DesignFragment;
+import com.moringaschool.my_portfolioapp.Fragments.websitesFragment;
+
 public class PortfolioActivity extends AppCompatActivity {
-    TextView Services, Home, Skills, Contact;
+    TextView Services, Home, Skills, Contact, Design, Website, AndroidApps;
+    private FrameLayout framelayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
 
+        Design = findViewById(R.id.Design);
+        Website = findViewById(R.id.Websites);
+        AndroidApps = findViewById(R.id.AndroidApps);
+
         Home = findViewById(R.id.Home);
         Services = findViewById(R.id.Services);
         Skills = findViewById(R.id.Skills);
         Contact = findViewById(R.id.Contact);
+        framelayout = findViewById(R.id.frameLayout);
 
         Home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,5 +57,31 @@ public class PortfolioActivity extends AppCompatActivity {
                 startActivity(new Intent(PortfolioActivity.this,ContactActivity.class));
             }
         });
+
+        Design.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new DesignFragment());
+            }
+        });
+        AndroidApps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new AndroidAppsFragment());
+            }
+        });
+        Website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new websitesFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
